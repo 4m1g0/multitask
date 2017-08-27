ec3778d1097cb7004206e0925817b865e426bf27
Cristian <cristian@localhost.localdomain>
2017-08-27 21:39:07 +0200

    Implemented SubworldTest that draws an animated ball

--- Alpheus/app/src/main/java/com/hewid/alpheus/Controller/Pacemaker.java
+++ Alpheus/app/src/main/java/com/hewid/alpheus/Controller/Pacemaker.java
@@ -22,8 +22,8 @@ public class Pacemaker implements FrameManager {
 
     public void start() {
         drawThread = new DrawThread();
-        drawThread.start();
         updateThread = new UpdateThread();
+        drawThread.start();
         updateThread.start();
         isRunning = true;
     }
--- Alpheus/app/src/main/java/com/hewid/alpheus/Model/Game/SubworldTest.java
+++ Alpheus/app/src/main/java/com/hewid/alpheus/Model/Game/SubworldTest.java
@@ -1,21 +1,35 @@
 package com.hewid.alpheus.Model.Game;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 
 class SubworldTest extends World {
+    private Bitmap catchedBitmap;
+    private int[] position = new int[2];
+    private float[] speed = new float[]{0.5f, 0.5f};
+
     public SubworldTest(int height, int width) {
         super(height, width);
+
+        catchedBitmap = Bitmap.createBitmap(100,100,Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(catchedBitmap);
+        Paint paint = new Paint();
+        paint.setColor(Color.rgb(12,200,170));
+        c.drawCircle(50,50,50,paint);
     }
 
     @Override
     public void update(long time) {
-
+        position[0] = (int)((speed[0] * time) % (width - 100));
+        position[1] = (int)((speed[1] * time) % (height - 100));
     }
 
     @Override
     public void draw(Canvas canvas) {
-
+        canvas.drawBitmap(catchedBitmap, position[0], position[1], null);
     }
 
     @Override
